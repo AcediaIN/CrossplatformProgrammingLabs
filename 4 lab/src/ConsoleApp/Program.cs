1ff93b34732c3696f6da4ef0e1462f8aa9f1e758
AcediaIN <104904276+AcediaIN@users.noreply.github.com>
2023-10-19 22:14:49 +0300

    Add files via upload

---
+++ 4 lab/src/ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿using Common;
+using JustCli;
+
+LabExecutor.Labs.Add(Lab1.Lab.Instance);
+LabExecutor.Labs.Add(Lab2.Lab.Instance);
+LabExecutor.Labs.Add(Lab3.Lab.Instance);
+
+await CommandLineParser.Default.ParseAndExecuteCommandAsync(args);
